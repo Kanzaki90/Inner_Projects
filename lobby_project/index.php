b610316d47fe2b6c437ed2de8d5a5ed50f6e3d2f
Kanzaki90 <antonhanin7@gmail.com>
2020-05-06 17:44:13 +0300

    msg

--- lobby_project/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_GET["url"])) {
-    $url = $_GET["url"];
-    include_once("./server/views/" . $url);
-} else
-    include_once("./server/views/login.html");
--- lobby_project/server/models/BaseModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-class BaseModel
-{
-    public function getConnector()
-    {
-        return $connection = new mysqli("localhost", "root", "", "lobby_schema");
-    }
-}
